d79f4233f1a03ed15bf0b0af3d772de5836ef1f8
DiegoWSL <diego.castroalves0@gmail.com>
2023-03-02 17:15:12 -0300

    iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+//
 function App() {
   return (
     <div className="meals">
